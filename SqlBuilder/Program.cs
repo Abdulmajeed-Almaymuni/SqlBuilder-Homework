92a04242e2696762429403f7641246fb60e97863
Abdulmajeed Almaymuni <Abdulmajeed.Almaymuni@tuwaiq.edu.sa>
2021-04-19 01:06:07 +0300

    Add project files.

---
+++ SqlBuilder/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SqlBuilder
+{
+    class Program
+    {
+       public class Query
+       {
+            string query;
+            public Query()
+            {
+                this.query = "SELECT ";
+            }
+
+            public Query Fields(string [] array)
+            {
+                foreach (var item in array)
+                {
+                    this.query += item + ", ";
+                }
+                this.query = this.query.Substring(0, this.query.Length - 2);
+                    return this;
+            }
+
+            public Query Table(string table)
+            {
+                this.query += " FROM " + table;
+                return this;
+            }
+
+            public Query Where(string where)
+            {
+                this.query += "\nWHERE " + where;
+                return this;
+            }
+
+            public string getQuery()
+            {
+                return this.query;
+            }
+
+       }
+        static void Main(string[] args)
+        {
+            Query query = new Query();
+            string[] fields = { "ID", "Name" };
+            query.Fields(fields).Table("Tuwaiq").Where("Name = Abdulmajeed Almaymuni");
+            Console.WriteLine(query.getQuery());
+        }
+    }
+}
